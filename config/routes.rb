3c8fbb77ce3c302fbe57b65859a08617845f7080
Camden <camdencparker@gmail.com>
2022-03-09 07:24:24 -0700

    adds restful routes for remaining models

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,23 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   get "/users/:id" => "users#show"
 
+  #Listing routes
+  get "/listings" => "listings#index"
+  post "/listings" => "listings#create"
+  get "/listings/:id" => "listings#show"
+  patch "/listings/:id" => "listings#update"
+  delete "/listings/:id" => "listings#destroy"
+
+  #Conversation routes
+  get "/conversations" => "conversations#index"
+  post "/conversations" => "conversations#create"
+  get "/conversations/:id" => "conversations#show"
+  delete "/conversations/:id" => "conversations#destroy"
+
+  #Message routes
+  get "/messages" => "messages#index"
+  post "/messages" => "messages#create"
+
   #Authentication
   post "/sessions" => "sessions#create"
 
