c60e85efac321e661ab0be876c32c929ab52839e
Camden <camdencparker@gmail.com>
2022-03-14 10:46:47 -0700

    adds associated data to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :phone, :location, :profile_pic
+
+  has_many :conversations
+  has_many :listings
+  has_many :messages
 end
